006f88e0f047149d80b4963516110ddf02c6101e
Luigi Assenza <lassenza@adaptavist.com>
2018-05-05 18:55:11 +0100

    Added main-container, upper-container, left-container and central-container

--- src/main/webapp/js/cms.js
+++ src/main/webapp/js/cms.js
@@ -3,22 +3,22 @@ window.onload = function(){
 	document.getElementById("save-button").setAttribute("hidden","hidden");
 	document.getElementById("cancel-button").setAttribute("hidden","hidden");
 	function edit() {
-		document.getElementById("container").setAttribute("contenteditable","true");
-		document.getElementById("container").setAttribute("class", "edit-container");
+		document.getElementById("editable-container").setAttribute("contenteditable","true");
+		document.getElementById("editable-container").setAttribute("class", "edit-container");
 		document.getElementById("save-button").removeAttribute("hidden");
 		document.getElementById("cancel-button").removeAttribute("hidden");
 		document.getElementById("edit-button").setAttribute("hidden","hidden");
 	}
 	function save(){
-		document.getElementById("container").setAttribute("contenteditable","false");
-		document.getElementById("container").removeAttribute("class");
+		document.getElementById("editable-container").setAttribute("contenteditable","false");
+		document.getElementById("editable-container").removeAttribute("class");
 		document.getElementById("save-button").setAttribute("hidden","hidden");
 		document.getElementById("cancel-button").setAttribute("hidden","hidden");
 		document.getElementById("edit-button").removeAttribute("hidden");
 	}
 	function cancel(){
-		document.getElementById("container").setAttribute("contenteditable","false");
-		document.getElementById("container").removeAttribute("class");
+		document.getElementById("editable-container").setAttribute("contenteditable","false");
+		document.getElementById("editable-container").removeAttribute("class");
 		document.getElementById("cancel-button").setAttribute("hidden","hidden");
 		document.getElementById("save-button").setAttribute("hidden","hidden");
 		document.getElementById("edit-button").removeAttribute("hidden");
@@ -37,7 +37,7 @@ window.onload = function(){
 	function getHTML(){
 		var lines = "";
 		
-		var children  = document.getElementById('container').children;
+		var children  = document.getElementById("editable-container").children;
 		
         for (i = 0; i <= children.length - 1; i++) {	
 			var line = children[i].outerHTML;
@@ -47,7 +47,7 @@ window.onload = function(){
 	}
 	ajax();
 	function ajax(){
-		var url = "storage.txt";
+		var url = "";
 		var http = new XMLHttpRequest();
 		http.open("POST", url, true);
 		http.setRequestHeader('Content-Type', 'text/plain; charset=ISO-8859-1');
